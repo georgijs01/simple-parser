9728dd3cd1e4109b26420b97db3ca0b46769cbdd
georgijs01 <48869301+georgijs01@users.noreply.github.com>
2020-02-02 17:11:51 +0100

    continued work on parsing

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,10 +1,11 @@
-use crate::tokenizer::Func::{Sine, Cosine, Tangent, Exp};
-use crate::tokenizer::Token::{Var, Const, OpenBrace, CloseBrace, Assign};
+use crate::tokenizer::Token::{Var, Const, OpenBrace, CloseBrace, Assign, Func, Op};
+use crate::tokenizer::FuncType::{Sine, Cosine, Tangent, Exp};
+use crate::tokenizer::OpType::{Sub, Add, Mult, Div};
 
 pub struct Tokenizer {
 
     data: Vec<char>,
-    current: usize,
+    current: isize,
 
 }
 
@@ -15,8 +16,17 @@ impl Tokenizer {
     }
 
     pub fn next(&mut self) -> Option<Token> {
+
+        // increment the current counter, as this is not done before this method returns
+        self.current += 1;
+
+        // skip whitespace
+        while self.current < self.data.len() as isize && self.data[self.current] == ' ' {
+            self.current += 1;
+        }
+
         // first try to match any single character values
-        match self.data[self.current] {
+        match self.data[self.current as usize] {
             '(' => return Some(OpenBrace),
             ')' => return Some(CloseBrace),
             '=' => return Some(Assign),
@@ -24,12 +34,56 @@ impl Tokenizer {
         }
 
         let mut state = State::Undef;
+        let start = self.current as usize;
+        let mut read = String::new();
 
-        loop {
-
+        while self.current < self.data.len() as isize {
+            match (&mut state, self.data[self.current as usize]) {
+                (State::Undef, '0' ..= '9') => state = State::Const,
+                (State::Undef, 'A' ..= 'z') => state = State::Name,
+                (State::Undef, x @ _) => {
+                    match x {
+                        '+' => return Some(Op(Add)),
+                        '*' => return Some(Op(Mult)),
+                        '/' => return Some(Op(Div)),
+                        '-' => {
+                            if self.current + 1 < self.data.len() as isize
+                                && self.data[self.current as usize + 1].is_digit(10) {
+                                state = State::Const;
+                            } else {
+                                return Some(Op(Sub))
+                            }
+                        },
+                        _ => return None,
+                    }
+                }
+                (State::Name, '(') => {
+                    return match read.as_str() {
+                        "sin" => Some(Func(Sine)),
+                        "cos" => Some(Func(Cosine)),
+                        "tan" => Some(Func(Tangent)),
+                        "exp" => Some(Func(Exp)),
+                        _ => None,
+                    }
+                },
+                (State::Name, x @ _) => {
+                    if !x.is_alphabetic() {
+                        return Some(Var(read));
+                    }
+                }
+                (State::Const, x @ _) => {
+                    if !x.is_digit(10) {
+                        return Some(Const(read.parse::<i64>().unwrap()))
+                    }
+                }
+                _ => (),
+            }
+            read.push(self.data[self.current]);
+            self.current += 1;
         }
-    }
 
+        return None;
+    }
 }
 
 pub enum State {
@@ -44,15 +98,15 @@ pub enum Token {
 
     Var(String),
     Const(i64),
-    Func(Func), // includes an opening brace after it, i.e. must be closed by a CloseBrace
+    Func(FuncType), // includes an opening brace after it, i.e. must be closed by a CloseBrace
     OpenBrace,
     CloseBrace,
     Assign,
-    Op(Op),
+    Op(OpType),
 
 }
 
-pub enum Func {
+pub enum FuncType {
 
     Sine,
     Cosine,
@@ -61,7 +115,7 @@ pub enum Func {
 
 }
 
-pub enum Op {
+pub enum OpType {
 
     Add,
     Sub,
